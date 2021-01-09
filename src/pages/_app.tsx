b0d81e0a3a4b2b14a46048979ffba52a94887fb6
輿石ゆうり <yuri.koshiishi@koshiishiyuuris-MacBook-Pro.local>
2021-01-09 14:15:43 +0900

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {AppProps} from 'next/app';
-import Router from 'next/router';
+import Router, {useRouter} from 'next/router';
 import '../../styles/globals.css';
 import '../../styles/nprogress.css';
 import 'slick-carousel/slick/slick.css';
@@ -11,6 +11,7 @@ import theme from '../theme';
 import NProgress from 'nprogress';
 import {AuthProvider} from '../contexts/auth';
 import Error from './_error';
+import * as gtag from '../utils/gtag';
 
 Router.events.on('routeChangeStart', (url) => {
   NProgress.start();
@@ -19,6 +20,18 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({Component, pageProps}: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
