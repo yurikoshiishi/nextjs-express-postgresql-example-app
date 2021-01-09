de30b20a4959bf77ecf84a8131d1621583f06b46
輿石ゆうり <yuri.koshiishi@koshiishiyuuris-MacBook-Pro.local>
2021-01-09 16:08:56 +0900

    updated api restristion

--- src/pages/api/pages/index.ts
+++ src/pages/api/pages/index.ts
@@ -1,13 +1,14 @@
 import {getHomePage} from '../../../sql';
 import db from '../../../utils/db';
-import {cors} from '../../../utils/middlewares';
 
 const NUMBER_OF_ITEMS = 5;
 const NUMBER_OF_REVIEWS = 3;
 
 export default async (req, res) => {
-  await cors(req, res);
   try {
+    if (!req.headers.host.includes(process.env.HOST)) {
+      return res.status(401).end();
+    }
     const data = await db.instance.one(getHomePage, {
       numberOfItems: NUMBER_OF_ITEMS,
       numberOfReviews: NUMBER_OF_REVIEWS,
--- src/pages/api/reviews/index.ts
+++ src/pages/api/reviews/index.ts
@@ -1,12 +1,13 @@
 import db from '../../../utils/db';
 import {getReviews} from '../../../sql';
-import {cors} from '../../../utils/middlewares';
 
 const PER_PAGE = 10;
 
 export default async (req, res) => {
-  await cors(req, res);
   try {
+    if (!req.headers.host.includes(process.env.HOST)) {
+      return res.status(401).end();
+    }
     const reviews = await db.instance.any(getReviews, {
       perPage: PER_PAGE,
       currentPage: req.query.page || 1,
