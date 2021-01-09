74b856177fcd87260e6ea9aed88c600fe07fdcfa
輿石ゆうり <yuri.koshiishi@koshiishiyuuris-MacBook-Pro.local>
2021-01-09 14:32:18 +0900

    updated review styling. updated gtag to handle error

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,6 +2,9 @@ export const GA_TRACKING_ID = 'G-9R1NXPZWZF';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
+  if (!window.gtag) {
+    return;
+  }
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
@@ -16,6 +19,9 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({action, category, label, value}: GTagEvent) => {
+  if (!window.gtag) {
+    return;
+  }
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
