caeb9faf5284c8f9c78125d41db79453adb9e96b
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2021-06-19 22:04:32 +0900

    delete: unnecessary files and codes

--- client/src/pages/reviews/my-reviews.tsx
+++ client/src/pages/reviews/my-reviews.tsx
@@ -1,12 +1,9 @@
-import {NextPageContext} from 'next';
 import React from 'react';
 import MyReviewList from '../../components/containers/MyReviewList';
 import PageWithImage from '../../components/containers/PageWithImage';
 import FirebaseLogin from '../../components/elements/FirebaseLogin';
 import SidebarLayout from '../../components/layouts/SiderbarLayout';
 import {useAuth} from '../../contexts/auth';
-import nookies from 'nookies';
-import firebaseAdmin from '../../utils/firebaseAdmin';
 
 interface MyReviewsProps {
   [key: string]: any;
